dd4c3355b83ba93980e27ab11a10aba34ebc7f92
shoheitada <shouheitada@smartstudy.co.jp>
2020-10-18 16:00:53 +0900

    課題-いろいろなフォームの入力欄
    課題作成

---
+++ 2-12/index.php
@@ -0,0 +1,17 @@
+<form action="result.php" method="post">
+      お名前：<input type="text" name="my_name" /><br>
+      ご希望商品：
+      <input type="radio" name="syou" value="A賞">A賞
+      <input type="radio" name="syou" value="B賞">B賞
+      <input type="radio" name="syou" value="C賞">C賞
+    <br>
+      <select name="number">
+       <option value="1">1</option>
+       <option value="2">2</option>
+       <option value="3">3</option>
+       <option value="4">4</option>
+       <option value="5">5</option>
+      </select>
+    <br>
+      <input type="submit" value="申込" />
+    </form>
---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+    $my_name = $_POST['my_name'];
+    $syou = $_POST['syou'];
+    $number = $_POST['number'];
+?>
+    <p>お名前：<?php echo $my_name; ?></p>
+    <p>ご希望商品<?php echo $syou; ?></p>
+    <p>個数：<?php echo $number; ?></p>
