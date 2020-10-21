b454aedb0cea24dc7c02066a143aaa2a54e545c5
shoheitada <shouheitada@smartstudy.co.jp>
2020-10-21 14:56:19 +0900

    課題-再提出
    課題作成

--- 2-18/answer.php
+++ 2-18/answer.php
@@ -8,22 +8,28 @@ $answer1 = $_POST['answer1'];
 $answer2 = $_POST['answer2'];
 $answer3 = $_POST['answer3'];
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
-if($port == $answer1){
-	$result1 = "正解！";
-}else{
-	$result1 = "残念･･･";
+function result1($port,$answer1){
+	if($port == $answer1){
+		print "正解！";
+	}else{
+		print "残念･･･";
+    }
 }
 
-if($gengo == $answer2){
-	$result2 = "正解！";
-}else{
-	$result2 = "残念･･･";
+function result2($gengo,$answer2){
+	if($gengo == $answer2){
+		print "正解！";
+	}else{
+		print "残念･･･";
+    }
 }
 
-if($comand == $answer3){
-	$result3 = "正解！";
-}else{
-	$result3 = "残念･･･";
+function result3($comand,$answer3){
+	if($comand == $answer3){
+		print "正解！";
+	}else{
+		print "残念･･･";
+    }
 }
 ?>
 <link rel="stylesheet" href="style.css">
@@ -31,12 +37,12 @@ if($comand == $answer3){
 <p><!--POST通信で送られてきた名前を表示--><?php echo $my_name; ?>さんの結果は・・・？</p>
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?php echo $result1 ?>
+<?php result1($port,$answer1);?>
 
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?php echo $result2 ?>
+<?php result2($gengo,$answer2);?>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?php echo $result3 ?>
+<?php result3($comand,$answer3);?>
 </body>
