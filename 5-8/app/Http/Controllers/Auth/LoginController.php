0800a4f0e406164a3d1613bcfe00b9a16c313b7d
shoheitada <shouheitada@smartstudy.co.jp>
2020-11-29 14:37:42 +0900

    modify transfer

--- 5-8/app/Http/Controllers/Auth/LoginController.php
+++ 5-8/app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = 'post/create';
 
     /**
      * Create a new controller instance.
